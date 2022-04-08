19bbb8c9d6bd75f64103ecaa5d09732483021291
Alizare0017 <alizare0017@gmail.com>
2022-04-08 16:03:43 +0430

    Update settings.py

--- Personal/settings.py
+++ Personal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z2&($w6(k!+5j$vkpf$ey&+btbyb8#ij7@@y3$u1zzot=g4w0='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['alizare.herokuapp.com','127.0.0.1']
 
